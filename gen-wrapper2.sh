7dfa87ca95d805eba781ec3fd620f1d872532e4c
Aitor Pazos <mail@aitorpazos.es>
2026-03-18 22:58:15 +0000

    chore: prepare for public release
    - Remove development scaffolding scripts (build-apk.sh, push-to-github.sh, fix-wrapper.sh, gen-wrapper.sh, gen-wrapper2.sh)
    - Add personal project disclaimer to README
    - Change clone URL from SSH to HTTPS in README

--- gen-wrapper2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /Users/aitor/zeroclaw-agents/personal/workspace/piper-tts-android
-export JAVA_HOME=/opt/homebrew/opt/openjdk@17
-export ANDROID_HOME=/Users/aitor/Library/Android/sdk
-rm -f gradle/wrapper/gradle-wrapper.jar gradlew gradlew.bat
-/opt/homebrew/bin/gradle wrapper --gradle-version 8.5
-echo "Result:"
-ls -la gradlew gradle/wrapper/
